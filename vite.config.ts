1187952f98acdf2cc317ac464f679f66a7936446
Michael Hess <mrhess25@gmail.com>
2024-04-29 21:01:53 +0000

    404 added; YAML file added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// For TypeScript, you might also import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react()],
-  // For TypeScript: plugins: [react(), tsconfigPaths()]
+  base: "/repo-name/",
 });
